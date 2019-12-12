94f9b9ad162cd073ef6fd2fabe97eee6476e02f8
Danilo Bassi <danilo-bassi@hotmail.com>
2019-12-11 22:31:55 -0300

    commit post testeada del milo

--- Joestick/Assets/chidori_particulas_controller.cs
+++ Joestick/Assets/chidori_particulas_controller.cs
@@ -12,8 +12,7 @@ public class chidori_particulas_controller : MonoBehaviour
     void Start()
     {
         this.transform.position = master.transform.position;
-    
-
+        GameObject.Destroy(this,10f);
     }
 
     // Update is called once per frame
