eba0ddd0989750ee77fe0fff6a6c3879418dc158
Danilo Bassi <danilo-bassi@hotmail.com>
2019-12-26 15:58:38 -0300

    Funcionalidad agregada al menu

--- Joestick/Assets/MainMenu_controller.cs
+++ Joestick/Assets/MainMenu_controller.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu_controller : MonoBehaviour
 {
+    public GameObject camara;
+    private Animator camara_anim;
+
+    public void Start()
+    {
+        camara_anim = camara.GetComponent<Animator>();
+    }
 
     public void QuitGame()
     {
@@ -11,7 +19,7 @@ public class MainMenu_controller : MonoBehaviour
     }
     public void PlayGame()
     {
-
+        SceneManager.LoadScene("EleccionPersonajes");
     }
     public void Creditos()
     {
@@ -19,7 +27,22 @@ public class MainMenu_controller : MonoBehaviour
     }
     public void Controles()
     {
+        camara_anim.SetTrigger("tocontrol");
+        Invoke("ActivaControles", 2);
+    }
 
+    public void ActivaControles()
+    {
+        this.transform.Find("CONTROLES").gameObject.SetActive(true);
+    }
+    public void ControlesToMenu()
+    {
+        camara_anim.SetTrigger("controlTOmain");
+        Invoke("ActivaMenu", 2);
+    }
+    public void ActivaMenu()
+    {
+        this.transform.Find("MAIN").gameObject.SetActive(true);
     }
 
 
