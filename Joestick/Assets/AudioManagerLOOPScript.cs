05e7f7a3558146ad07f2fe0bd39c65f2ba6d674d
Danilo Bassi <danilo-bassi@hotmail.com>
2020-01-08 15:20:16 -0300

    Agregado de animaciones de nievla

---
+++ Joestick/Assets/AudioManagerLOOPScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManagerLOOPScript : MonoBehaviour
+{
+
+    public bool EnLoop = true;
+
+    public Sound[] sounds;
+
+    // Use this for initialization
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volumen;
+            s.source.pitch = s.pitch;
+            s.source.loop = EnLoop;
+
+        }
+    }
+
+    void Start()
+    {
+        Play("viento");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.clip.name == name);
+        s.source.Play();
+    }
+}
